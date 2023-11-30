9b1d46e69b8ba81e7c0ce569d22d2488687f917d
JakubKermes <jakubker22@gmail.com>
2023-11-30 20:51:43 +0100

    Color recognition

---
+++ FillFolders.py
@@ -0,0 +1,43 @@
+from bing_image_downloader import downloader
+import os
+import shutil
+
+def download_images(keyword, output_path, limit=100):
+    downloader.download(keyword, limit=limit, output_dir=output_path, adult_filter_off=True, force_replace=False)
+
+    # Move images from subdirectory to the desired location
+    subdirectory = os.path.join(output_path, keyword)
+    for file_name in os.listdir(subdirectory):
+        src_path = os.path.join(subdirectory, file_name)
+        dest_path = os.path.join(output_path, file_name)
+
+        # Check if the file already exists in the destination path
+        if os.path.exists(dest_path):
+            # You can choose to skip or replace the existing file
+            # Here, I'm replacing the existing file
+            os.remove(dest_path)
+
+        shutil.move(src_path, dest_path)
+
+    # Remove the unnecessary subdirectory
+    os.rmdir(subdirectory)
+
+def fill_folders_with_images(root_path, categories, limit_per_category=1):
+    for split in ["train", "val", "test"]:
+        split_path = os.path.join(root_path, split)
+
+        for category in categories:
+            category_path = os.path.join(split_path, category)
+
+            # Create category directory if it doesn't exist
+            if not os.path.exists(category_path):
+                os.makedirs(category_path)
+
+            download_images(category, category_path, limit=limit_per_category)
+
+if __name__ == "__main__":
+    dataset_root = "/home/jakub/pytorch/dataset"  # Updated dataset root path with leading slash
+    categories = ["red", "blue", "green"]
+    limit_per_category = 100  # You can adjust the limit as needed
+
+    fill_folders_with_images(dataset_root, categories, limit_per_category)
---
+++ main.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+# Define a simple neural network for color recognition
+class ColorRecognitionModel(nn.Module):
+    def __init__(self):
+        super(ColorRecognitionModel, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.fc = nn.Linear(128 * 56 * 56, num_classes)  # Adjust input size based on your image size
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+# Define transformations and datasets
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+train_dataset = datasets.ImageFolder(root='/home/jakub/pytorch/dataset/train', transform=transform)
+val_dataset = datasets.ImageFolder(root='/home/jakub/pytorch/dataset/val', transform=transform)
+test_dataset = datasets.ImageFolder(root='/home/jakub/pytorch/dataset/test', transform=transform)
+
+batch_size = 32
+num_classes = 10  # Adjust based on the number of color classes
+
+# Create data loaders
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+# Initialize the model, loss function, and optimizer
+model = ColorRecognitionModel()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training loop
+num_epochs = 10
+
+for epoch in range(num_epochs):
+    model.train()
+    for inputs, labels in train_loader:
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+    # Validation loop
+    model.eval()
+    val_loss = 0.0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in val_loader:
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Epoch {epoch+1}/{num_epochs}, Loss: {val_loss/len(val_loader)}, Accuracy: {100*correct/total}%')
+
+# Test the model
+model.eval()
+test_correct = 0
+test_total = 0
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        test_total += labels.size(0)
+        test_correct += (predicted == labels).sum().item()
+
+print(f'Test Accuracy: {100*test_correct/test_total}%')
